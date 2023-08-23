cfa2a2d4d6da4ce23b3b5195ab2c068c1c99ce1a
一时 三月 <68946023+LittleFIve233@users.noreply.github.com>
2023-08-24 01:27:24 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <coreinit/thread.h>
 #include <coreinit/debug.h>
-#include <coreinit/time.h>
 
 #include <whb/proc.h>
 #include <whb/log.h>
@@ -21,8 +20,6 @@ int main(int argc, char **argv) {
 			uint64_t titleId = 0x0005000010132200;  // 如果你想更改自启动游戏，请更改此条
 			_SYSLaunchTitleWithStdArgsInNoSplash(titleId, nullptr);
 		} else {
-			OSCalendarTime tm;
-            OSTicksToCalendarTime(OSGetTime(), &tm);
 			OSFatal("Failed to launch the specified APP!\nPlease check if this software exists!\nPress the power button for 4 seconds to turn off the console.\n\nThis feature comes from Menuboot Module.\nMenuBoot Module Created by LittlFIve.\nLastest build Time: 2023/08/24");
             /* 正在思考如何正确报错输出信息并返回WiiU Menu，以下为等待后返回代码
 			OSSleepTicks(OSMillisecondsToTicks(3000));
