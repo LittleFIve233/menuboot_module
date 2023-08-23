22a10ee1286a1133cfc8feb93f70b39c25eb4aa4
一时 三月 <68946023+LittleFIve233@users.noreply.github.com>
2023-08-23 12:15:05 +0800

    修正main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main() {
 			uint64_t titleId = 0x0005000010132200;  // 如果你想更改自启动游戏，请更改此条
 			_SYSLaunchTitleWithStdArgsInNoSplash(titleId, nullptr);
 		} else {
-			OSFatal("Failed to boot the APP!\nThis Module From LittlFIve.\nCreate on 2023/08/23");
+			OSFatal("Failed to boot the APP!\nThis Module From LittlFIve.");
 		}
 
 		while(WHBProcIsRunning()) {}
