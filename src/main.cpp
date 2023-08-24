b3f96e0e4d5a9179ed6a9a9cb9de86bed22cb88c
一时 三月 <68946023+LittleFIve233@users.noreply.github.com>
2023-08-24 17:30:40 +0800

    Version 1.0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,27 @@
 #include <coreinit/thread.h>
 #include <coreinit/debug.h>
-
 #include <whb/proc.h>
 #include <whb/log.h>
 #include <whb/log_console.h>
-
 #include <sysapp/title.h>
 #include <sysapp/launch.h>
 
 int main(int argc, char **argv) {
 	WHBProcInit();
-	WHBLogConsoleInit();
 	
 	if (WHBProcIsRunning()) {
 		if (
-			SYSCheckTitleExists(0x0005000010132200) // 检查Id是否存在
+			SYSCheckTitleExists(0x00050000101d3000) // 检查Id是否存在
 		)
 		{
-			uint64_t titleId = 0x0005000010132200;  // 如果你想更改自启动游戏，请更改此条
+			uint64_t titleId = 0x00050000101d3000; // 如果你想更改自启动游戏，请更改此条
 			_SYSLaunchTitleWithStdArgsInNoSplash(titleId, nullptr);
 		} else {
 			OSFatal("Failed to launch the specified APP!\nPlease check if this software exists!\nPress the power button for 4 seconds to turn off the console.\n\nThis feature comes from Menuboot Module.\nMenuBoot Module Created by LittlFIve.\nLastest build Time: 2023/08/24");
-            /* 正在思考如何正确报错输出信息并返回WiiU Menu，以下为等待后返回代码
-			OSSleepTicks(OSMillisecondsToTicks(3000));
-			SYSLaunchMenu();
+            /*
+			WHBLogConsoleDraw(); // 使用屏幕渲染颜色的方法告知无法找到Title
+			OSSleepTicks(OSMillisecondsToTicks(5000)); // 等待秒数，单位为毫秒
+			SYSLaunchMenu(); // 返回WiiU Menu
 			*/
 		}
 
